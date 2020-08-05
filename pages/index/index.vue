24f8db4c52a08a2f068d9ffdfbb73f7e01e88244
longerX <1620107659@qq.com>
2020-08-05 11:59:12 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,64 @@
+<template>
+	<view class="container">
+		<!-- 搜索框 -->
+		<view class="search">
+			<input type="text" placeholder="请输入搜索内容" style="width:80%;display:inline-block;height:30px;font-size:18px;"/>
+			<button type="default">搜索</button>
+		</view>
+		<!-- 轮播图 -->
+		<view class="lunbo">
+			<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" circular="true">
+				<swiper-item>
+					<image src="https://aecpm.alicdn.com/simba/img/TB1X6uHLVXXXXcCXVXXSutbFXXX.jpg" mode=""></image>
+				</swiper-item>
+				<swiper-item>
+					<image src="https://aecpm.alicdn.com/simba/img/TB13xKuLVXXXXcHapXXSutbFXXX.jpg" mode=""></image>
+				</swiper-item>
+				<swiper-item>
+					<image src="//img.alicdn.com/tps/i4/TB1GrDgf8Bh1e4jSZFhSuwC9VXa.jpg_240x240q90.jpg" mode=""></image>
+				</swiper-item>
+			</swiper>
+		</view>
+		<view class="" style="margin-top:20px;">
+			<view style="font-size:18px;font-weight:500;">您目前的位置是</view>
+			<map :latitude="latitude" :longitude="longitude"></map>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.container {
+		font-size: 14px;
+		line-height: 24px;
+	}
+	.search{
+		margin-top:10px;
+		padding-left:15px;
+	}
+	.search button{
+		float:right;
+		width:20%;
+		height:30px;
+		line-height:30px;
+	}
+	.lunbo image{
+		width:100%;
+	}
+	map{
+		width:100%;
+		height:100px;
+	}
+</style>
---
+++ pages/me/me.vue
@@ -0,0 +1,35 @@
+<template>
+	<view class="">
+		<input type="text" value="" placeholder="请输入用户名"/>
+		<input type="text" value="" password="true" placeholder="请输入密码"/>
+		<view class="">
+			<button type="default">登陆</button>
+			<button type="default">注册</button>
+		</view>
+		<image src="https://dss2.bdstatic.com/8_V1bjqh_Q23odCf/pacific/1964126218.jpg" mode=""></image>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+	input{
+		width:100%;
+		height:30px;
+		font-size:18px;
+		padding-left:10px;
+		margin:5px 0;
+	}
+	button{
+		float:left;
+		width:20%;
+		height:30px;
+		line-height:30px;
+		margin-left:5px;
+	}
+	image{
+		margin-top:20px;
+		width:100%;
+	}
+</style>
